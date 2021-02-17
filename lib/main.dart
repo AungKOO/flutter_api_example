68045dcb8ac7f9fb39950b6b256cc5638b795739
Aung KO <aungko.uit@gmail.com>
2021-02-18 00:49:11 +0630

    add logging and api

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ void _setupLogging() {
   Logger.root.level = Level.ALL;
   Logger.root.onRecord.listen((event) {
     print("${event.level.name}: ${event.time} : ${event.message}");
-  })
+  });
 }
 
 class MyApp extends StatelessWidget {
